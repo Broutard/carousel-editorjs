371136b58601328a1be3edd11a6ad5540612bb5b
seb <sr@corp.webedia.fr>
2022-06-26 21:30:10 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/dist'),
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: 'dist.js',
     library: 'Carousel',
     libraryTarget: 'umd',
     libraryExport: 'default'
